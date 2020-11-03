0a61cd500612a0f45f545dc9d7eb111786fd7bea
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-03 01:39:05 +0000

    Deploy lotabout/hexo-theme-noise to github.com/lotabout/hexo-theme-noise.git:gh-pages

---
+++ js/noise.js
@@ -0,0 +1,55 @@
+var nextPage = 2;
+var loading = false;
+var postsPerPage = $('.post-entry').length;
+var allPagesFetched = false;
+var loadingHTML = '<div class="loading"><div class="spinner"><div></div><div></div><div></div></div></div>';
+var loading = false;
+
+function startLoading() {
+  loading = true;
+  $('.entries').parent().append(loadingHTML)
+}
+
+function stopLoading() {
+  loading = false;
+  if ($('.loading')) {
+    $('.loading').remove()
+  }
+}
+
+function isLoading() {
+  return loading;
+}
+
+function appendPage() {
+  if (allPagesFetched || isLoading()) {
+    return;
+  }
+
+  startLoading();
+
+  $.get('/page/' + nextPage + '/')
+    .fail(function () {
+      allPagesFetched = true;
+    })
+    .done(function (data) {
+      nextPage += 1;
+
+      var div = $('<div/>').html(data).contents();
+      var posts = div.find('.post-entry');
+      $('.entries').append(posts);
+
+      if (posts.length < postsPerPage) {
+        allPagesFetched = true;
+      }
+    })
+    .always(function () {
+      stopLoading();
+    });
+}
+
+$(window).scroll(function () {
+   if ($(window).scrollTop() >= $(document).height() - $(window).height() - 16) {
+     appendPage();
+   }
+});
